963b96c21c680419f3948b31923821c29d889460
Shubham Prashar <sirprashar@gmail.com>
2022-04-15 12:42:00 +0530

    repo initiliazed

---
+++ src/main/java/io/github/oyeprashar/BloodBankSystem/controller/HomePage.java
@@ -0,0 +1,14 @@
+package io.github.oyeprashar.BloodBankSystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomePage {
+    @GetMapping("/")
+    public String index() {
+        return "HomePage";
+    }
+}
+
